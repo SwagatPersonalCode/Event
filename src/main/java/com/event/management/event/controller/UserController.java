ab8439b7d8aadecddf4f55eeecf336dc215a1f66
91824 <swagat.sahu1995@gmail.com>
2024-07-24 00:45:18 +0530

    Commit changes for removing Logger

--- src/main/java/com/event/management/event/controller/UserController.java
+++ src/main/java/com/event/management/event/controller/UserController.java
@@ -2,8 +2,6 @@ package com.event.management.event.controller;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +23,7 @@ import com.event.management.event.service.UserService;
 @RequestMapping("/user")
 public class UserController {
 
-    private static final Logger LOGGER= LoggerFactory.getLogger(UserController.class);
+//    private static final Logger LOGGER= LoggerFactory.getLogger(UserController.class);
 	private UserService userService;
 	
     @Autowired
@@ -36,7 +34,7 @@ public class UserController {
     
     @PostMapping("/createUser")
     public ResponseEntity<CommonResponse> createUser(@RequestBody UserDto userDto) {
-        LOGGER.info("UserController : createUser ");
+//        LOGGER.info("UserController : createUser ");
     	if(userService.createUser(userDto))
     		return ResponseEntity.status(HttpStatus.OK).body(new CommonResponse("200","User created successfully."));
     	
